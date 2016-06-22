8f1f9858b709de44c9f853297a00ee33b8ccf2f0
Maciej Matyjas <mmatyjas@expedia.com>
2016-06-22 21:58:07 +0100

    it was a bad token

--- lib/send_fb.ex
+++ lib/send_fb.ex
@@ -2,8 +2,7 @@ defmodule FinBot.SendFb do
 	def post(senderId, text) do
 		url = "https://graph.facebook.com/v2.6/me/messages?access_token=" <> System.get_env("FB_PAGE_ACCESS_TOKEN")
 		body = Poison.encode!(%{"recipient": %{"id": senderId}, "message": %{"text": text}})
-		IO.inspect(url)
-		IO.inspect(body)
+		# sometimes the token goes bad, like when I changed my password, returned a 400
 		HTTPotion.post url, [body: body, headers: ["Content-Type": "application/json"]]
 	end
 end
